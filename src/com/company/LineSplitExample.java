9db37f01dfc47f955e5cbe0152def880916b31b3
jfurgal <90795741+jfurgal@users.noreply.github.com>
2021-09-22 11:10:38 -0400

    project 1

--- src/com/company/LineSplitExample.java
+++ src/com/company/LineSplitExample.java
@@ -3,22 +3,33 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+
 public class LineSplitExample {
     public static void main(String[] args) throws IOException {
-        var fileName = "jerseys.txt";
-        var studentJerseys  = Paths.get(fileName);
-        var studentNames = new ArrayList<String>();
-        var jerseyNumbers = new ArrayList<Integer>();
-        var studentLines  = Files.readAllLines(studentJerseys);
-        for (var line : studentLines){
+        var fileName = "product.txt";
+        var productPrice  = Paths.get(fileName);
+        var productName = new ArrayList<String>();
+        var productCount = new ArrayList<Integer>();
+        var productLines  = Files.readAllLines(productPrice);
+        for (var line : productLines){
             var splitLine  = line.split(",");
-            studentNames.add(splitLine[0]);
-            var jerseyNum = Integer.parseInt(splitLine[1]);
-            jerseyNumbers.add(jerseyNum);
+            productPrice.add(splitLine[0]);
+            var productCount = Integer.parseInt(splitLine[1]);
+            productName.add(productNumber);
         }
-        for (int i = 0; i< studentNames.size(); i++){
-            System.out.println("Student: "+ studentNames.get(i) + " Jersey Number: " + jerseyNumbers.get(i));
-
+        for (int i = 0; i< productName.size(); i++){
+            System.out.println("Product: "+ productPrice.get(i) + " productAvailable: " + productPrice.get(i));
+            while(true) {
+                if (productName == productCount) {
+                    System.out.println("This is available" + productCount);
+                    break;
+                } else {
+                    if(productName> productCount)
+                        System.out.println("unfortunately " + productName +" is not available");
+                    else
+                        System.out.println("Unfortunately " +productName+ " is not available");
+                    System.out.print("What else would you like to look up?:");
+                    usersName = inputReader.nextInt();
         }
     }
 }
